90ea8eba69c8c3344b3293d520bba7fdc0d495dc
Esmail <elmoussel12@gmail.com>
2020-04-01 11:00:26 +0200

    Updates

---
+++ precache-manifest.437bcfd11bd19dd090a28bc66fd2b04d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d33ff5734eb20c2e318ba41c227708d",
+    "url": "/Twitter-App-Frontend/index.html"
+  },
+  {
+    "revision": "a16c6e5861758aeeed51",
+    "url": "/Twitter-App-Frontend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "f170d9c4e92e95ca3c98",
+    "url": "/Twitter-App-Frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a16c6e5861758aeeed51",
+    "url": "/Twitter-App-Frontend/static/js/2.18d56080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Twitter-App-Frontend/static/js/2.18d56080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f170d9c4e92e95ca3c98",
+    "url": "/Twitter-App-Frontend/static/js/main.32f0d96d.chunk.js"
+  },
+  {
+    "revision": "2a026f52ded1ce3ac0b1",
+    "url": "/Twitter-App-Frontend/static/js/runtime-main.9d9b5354.js"
+  },
+  {
+    "revision": "732dc8fe7fd55dbb1889e12c99822f89",
+    "url": "/Twitter-App-Frontend/static/media/avatar.732dc8fe.jpg"
+  },
+  {
+    "revision": "ae267f5150c6e145a1d18a30fc50ea3d",
+    "url": "/Twitter-App-Frontend/static/media/comment.ae267f51.png"
+  },
+  {
+    "revision": "f1782b1cdcc7d40fd0df91d2e45fed8d",
+    "url": "/Twitter-App-Frontend/static/media/delete.f1782b1c.webp"
+  },
+  {
+    "revision": "b9d889dd7a1c874e043ee86f6086a89a",
+    "url": "/Twitter-App-Frontend/static/media/like.b9d889dd.webp"
+  },
+  {
+    "revision": "39182f5fe8365a899557d5b4c5b61b4d",
+    "url": "/Twitter-App-Frontend/static/media/likes.39182f5f.jpg"
+  },
+  {
+    "revision": "d6716f1a7ac9e77140a8879f9cd0805b",
+    "url": "/Twitter-App-Frontend/static/media/logo.d6716f1a.png"
+  }
+]);
